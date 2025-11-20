d6d37480df4307cbac83d107fbd42fee5d180e45
keun66080823-ui <keun66080823@gmail.com>
2025-11-20 16:41:22 +0900

    TodoAdd 추가

---
+++ TodoList/src/main/java/edu/kh/todoList/controller/TodoAddServlet.java
@@ -0,0 +1,60 @@
+package edu.kh.todoList.controller;
+
+import java.io.IOException;
+import java.util.Map;
+
+import edu.kh.todoList.model.service.TodoListService;
+import edu.kh.todoList.model.service.TodoListServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/todo/add")
+public class TodoAddServlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		try {
+			// 1. TodoListServiceImpl 객체 생성
+			TodoListService service = new TodoListServiceImpl();
+			
+			// 2. 요청 시 전달받은 파라미터 데이터 얻어오기 (왜? DB의 TABLE에 추가하기 위해서. 다섯 요소 중 나머지 셋은 디폴트기에 데이터를 넘길 필요 없음.)
+			String title = req.getParameter("title");  // 제목
+			String detail = req.getParameter("detail");// 상세내용
+			
+			// 3. 서비스 호출 후 결과 반환받기
+			int result = service.todoAdd(title, detail);
+			
+			// 4. 성공/실패 메세지 세팅하기
+			String message = null;
+			if(result > 0) message = "추가 성공!!!";
+			else		   message = "추가 실패...";
+			
+			// 5. 기존 req를 사용할 수 없기 때문에
+			// session 을 이용해서 message를 세팅
+			HttpSession session = req.getSession();
+			session.setAttribute("message", message);
+			
+			// 6. 메인페이지로 redirect (재요청)
+			resp.sendRedirect("/");
+			// -> "/"는 최상위 경로로 재요청을 보냄.
+			// -> "/" 처리하는 Servlet/JSP 재요청. 즉 내부적으로는 index -> main -> mainServlet으로 재요청 된다.
+			// -> redirect는 무조건 GET 방식!!! 
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+}
